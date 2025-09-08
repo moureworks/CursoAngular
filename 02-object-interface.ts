b2feff2e52b8bae37a95948e18f56350853599e1
moureworks <moure96@gmail.com>
2025-09-08 14:41:54 +0200

    Upload files using Vice

---
+++ 02-object-interface.ts
@@ -0,0 +1,25 @@
+
+//const skills: string[] = ['Bash', 'Counter','Healing'];         //Array de strings
+//Si sabemos que la variable no va a cambiar, usar constante
+
+interface Character{
+    name: string;
+    hp: number;
+    skills: string[];
+    hometown?: string;                  //El interrogante lo hace opcional
+}
+
+
+const strider: Character ={             //Al asignarle la constante de tipo Character, lo que hago es tipar las caracteristicas del objeto que he definido en la interface
+    name: 'Strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+}
+
+
+strider.hometown='Rivendell';
+
+
+console.table(strider);
+
+export{}
